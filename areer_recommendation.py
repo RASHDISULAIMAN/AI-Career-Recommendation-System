d4d10492af344b9a1ecd9d438b8810719a118547
RASHDISULAIMAN <jabriomarrs@gmail.com>
2026-01-05 16:00:51 +0400

    Create areer_recommendation.py

---
+++ areer_recommendation.py
@@ -0,0 +1,15 @@
+number_of_courses = int(input("Enter number of courses: "))
+total = 0
+
+for i in range(number_of_courses):
+    grade = float(input(f"Enter grade for course {i + 1}: "))
+    total += grade
+
+average = total / number_of_courses
+
+if average >= 80:
+    career = "Engineering Specialist"
+else:
+    career = "Technical Support or Diploma Path"
+
+print("Recommended Career Path:", career)
